43e30dc4f00ea8e6c7a9cc297e3b967764f1b2f2
Michael Glas <michaelglas89@gmail.com>
2019-03-17 14:32:00 +0100

    added selection tinting added placing pieces added proper start mechanic added player specific piece images added default pieces added get_all_moves generator to clean up added capturing attribute to pieces to implement the pawn capturing added json files for pieces

--- src/ndchess/__init__.py
+++ src/ndchess/__init__.py
@@ -2,14 +2,36 @@ import numpy
 import itertools
 import operator
 from functools import reduce
+import json
+from os import path
+import os
 
-field = numpy.dtype([("player",numpy.uint8),("piece",numpy.uint16)])
+class PieceJsonEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, piece):
+            return {"directions":piece.directions,
+                     "max_moves":piece.directions,
+                     "auto_generate":piece.auto_generate,
+                     "images":piece.images,
+                     "start_max_moves":piece.start_max_moves,
+                     "capturing":piece.capturing if piece.has_capturing else None}
+        elif isinstance(o, numpy.ndarray):
+            return o.tolist()
+        json.JSONEncoder.default(self, o)
+
+def Piece_object_hook(d):
+    if "directions" in d:
+        return piece(**{ k:d[k] for k in ["directions","max_moves","auto_generate","images","start_max_moves","capturing"] if k in d})
+
+field = numpy.dtype([("player",numpy.uint8),("piece",numpy.uint8),("flags",numpy.uint8)])
+
+class flags(int):
+    MOVED = 2**0
 
 class hashable_array(numpy.ndarray):
     def __hash__(self):
         return int.from_bytes(self.tobytes(),"big")
 hashable_array.__eq__ = numpy.array_equal
-
 
 def ina(a,b):
     for i in a:
@@ -37,19 +59,92 @@ def find_piece2(piece,pieces):
             if r:
                 return r
 
+def pieces_from_file(file):
+    file = path.abspath(file)
+    if path.isfile(file):
+        wdir = os.getcwd()
+        os.chdir(path.dirname(file))
+    l = json.load(open(path.abspath(file),"r"),object_hook=Piece_object_hook)
+    os.chdir(wdir)
+    i = 0
+    length = len(l)
+    while i<length:
+        if not isinstance(l[i], piece):
+            l.pop(i)
+            length -= 1
+        i+=1
+    return l
+
+def _get_all_moves(pos,directions,board,player,max_moves):
+    for i in directions:
+        j = 1
+        new_pos = pos.copy()
+        while j<=max_moves:
+            new_pos += i
+            if (new_pos<0).any():
+                break
+            try:
+                cont2 = board[tuple(new_pos)]
+            except IndexError:
+                break
+            if cont2["player"]==player:
+                break
+            yield new_pos.copy()
+            if cont2["piece"]:
+                break
+            j+=1
+
+def _get_capturing_moves(pos,directions,board,player,max_moves):
+    for i in directions:
+        j = 1
+        new_pos = pos.copy()
+        while j<=max_moves:
+            new_pos += i
+            if (new_pos<0).any():
+                break
+            try:
+                cont2 = board[tuple(new_pos)]
+            except IndexError:
+                break
+            if cont2["player"]==player:
+                break
+            if cont2["piece"]:
+                yield new_pos.copy()
+                break
+            j+=1
+
+def _get_noncapturing_moves(pos,directions,board,player,max_moves):
+    for i in directions:
+        j = 1
+        new_pos = pos.copy()
+        while j<=max_moves:
+            new_pos += i
+            if (new_pos<0).any():
+                break
+            try:
+                cont2 = board[tuple(new_pos)]
+            except IndexError:
+                break
+            if cont2["player"]==player or cont2["piece"]:
+                break
+            yield new_pos.copy()
+            j+=1
+
 class ndChess:
     def __init__(self,shape,allowed_pieces):
         self.shape = shape
         self.allowed_pieces = [None]+list(map(operator.methodcaller("get_in_nd",len(shape)),allowed_pieces))
         self.board = numpy.zeros(shape,dtype=field)
         self.king_positions = set()
+        self.turn = 1
     def place_piece(self,pos,piece,player):
         if not isinstance(piece, int):
             piece = find_piece(piece, self.allowed_pieces)
         self.board[pos]["piece"] = piece
         if piece==1:
-            self.king_positions.add(numpy.array(pos).view(hashable_array))
+            self.king_positions.add((numpy.array(pos).view(hashable_array),player))
         self.board[pos]["player"] = player
+        self.board[pos]["flags"] = 0
     def get_piece(self,index):
         return numpy.take(self.allowed_pieces,self.board[index]["piece"])
     def move_piece(self,pos,new_pos,player):
@@ -58,10 +153,12 @@ class ndChess:
             cont = self.board[tuple(new_pos)]
             if (not cont["piece"]) or (cont["player"] != player):
                 self.board[new_pos] = cont1
+                self.board[new_pos]["flags"] |= flags.MOVED
                 self.board[pos] = b""
+                self.turn += 1
                 if cont["piece"]==1:
-                    self.king_positions.discard(pos.view(hashable_array))
-                    self.king_positions.add(new_pos.view(hashable_array))
+                    self.king_positions.discard((pos.view(hashable_array),player))
+                    self.king_positions.add((new_pos.view(hashable_array),player))
                 return True
         return False
     def is_check(self,player):
@@ -71,72 +168,143 @@ class ndChess:
             pp = cont["player"]
             if cont["piece"] and pp!=player:
                 piece = self.allowed_pieces[cont["piece"]]
-                for i in piece.directions:
-                    j = 1
-                    while j<=piece.max_moves:
-                        pos = wc+i*j
-                        if (pos<0).any():
-                            break
-                        try:
-                            cont2 = self.board[tuple(pos)]
-                        except IndexError:
-                            break
-                        if cont2["player"]==pp:
-                            break
-                        if cont2["piece"]==1 and cont2["player"]==player:
-                            return True
-                        if cont2["piece"]:
-                            break
-                        j+=1
-        return False
+                if not cont["flags"]&flags.MOVED:
+                    max_moves = piece.start_max_moves
+                else:
+                    max_moves = piece.max_moves
+                if piece.has_capturing:
+                    for i in piece.capturing:
+                        j = 1
+                        while j<=max_moves:
+                            pos = wc+i*j
+                            if (pos<0).any():
+                                break
+                            try:
+                                cont2 = self.board[tuple(pos)]
+                            except IndexError:
+                                break
+                            if cont2["player"]==pp:
+                                break
+                            if cont2["piece"]==1 and cont2["player"]==player:
+                                return piece
+                            if cont2["piece"]:
+                                break
+                            j+=1
+                else:
+                    for i in piece.directions:
+                        j = 1
+                        while j<=max_moves:
+                            pos = wc+i*j
+                            if (pos<0).any():
+                                break
+                            try:
+                                cont2 = self.board[tuple(pos)]
+                            except IndexError:
+                                break
+                            if cont2["player"]==pp:
+                                break
+                            if cont2["piece"]==1 and cont2["player"]==player:
+                                return piece
+                            if cont2["piece"]:
+                                break
+                            j+=1
     def is_checkmate(self,player):
-        coords = set(map(operator.methodcaller("view",hashable_array),itertools.chain.from_iterable(map(lambda x:self.get_all_moves(x, player),self.king_positions))))
+        coords = set(map(operator.methodcaller("view",hashable_array),itertools.chain.from_iterable(map(lambda x:self.get_all_moves(x[0], player),filter(lambda x:x[1]==player,self.king_positions)))))
         print(coords)
     def get_all_moves(self,pos,player):
+        if not self.board[tuple(pos)]["flags"]&flags.MOVED:
+            return self.get_all_moves_start(pos, player)
+        else:
+            return self._get_all_movesg(pos, player)
+    def _get_all_movesg(self,pos,player):
         cont = self.board[tuple(pos)]
         if cont["piece"] and cont["player"]==player:
             piece = self.allowed_pieces[cont["piece"]]
-            for i in piece.directions:
-                j = 1
-                while j<=piece.max_moves:
-                    pos = pos+i*j
-                    if (pos<0).any():
-                        break
-                    try:
-                        cont2 = self.board[tuple(pos)]
-                    except IndexError:
-                        break
-                    if cont2["player"]==player:
-                        break
-                    yield pos
-                    if cont2["piece"]:
-                        break
-                    j+=1
+            yield piece
+            if piece.has_capturing:
+                yield from _get_noncapturing_moves(pos, piece.directions, self.board, player, piece.max_moves)
+                yield from _get_capturing_moves(pos, piece.capturing, self.board, player, piece.max_moves)
+            else:
+                yield from _get_all_moves(pos, piece.directions, self.board, player, piece.max_moves)
+                    
+    def get_all_moves_start(self,pos,player):
+        cont = self.board[tuple(pos)]
+        if cont["piece"] and cont["player"]==player:
+            piece = self.allowed_pieces[cont["piece"]]
+            yield piece
+            if piece.has_capturing:
+                yield from _get_noncapturing_moves(pos, piece.directions, self.board, player, piece.start_max_moves)
+                yield from _get_capturing_moves(pos, piece.capturing, self.board, player, piece.max_moves)
+            else:
+                yield from _get_all_moves(pos, piece.directions, self.board, player, piece.start_max_moves)
+
+def extend(v,dims):
+    for sign in itertools.product((True,False),repeat=v.shape[0]):
+        sdirection = numpy.negative(v,where=sign,out=v.copy())
+        for i in itertools.permutations(range(dims),sdirection.shape[0]):
+            new_direction = numpy.zeros((dims,),dtype=sdirection.dtype)
+            new_direction[i,] = sdirection
+            yield new_direction
+    
 
 class piece:
-    def __init__(self, directions, max_moves=None, auto_generate = True, image=None):
+    def __init__(self, directions, max_moves=float("inf"), auto_generate = True, images=[], start_max_moves=None, capturing=None):
+        self.directions = list(map(numpy.array,directions))
+        self.max_moves = max_moves
+        self.auto_generate = auto_generate
+        self.images = list(map(path.abspath,images))
+        self.children = []
+        if start_max_moves is None:
+            self.start_max_moves = self.max_moves
+        else:
+            self.start_max_moves = start_max_moves
+        if capturing is None:
+            self.has_capturing = False
+        else:
+            self.has_capturing = True
+            self.capturing = list(map(numpy.array,capturing))
+    @classmethod
+    def from_values(cls,directions, max_moves, auto_generate, images, start_max_moves, has_capturing, capturing=None):
+        self = cls.__new__(cls)
         self.directions = directions
         self.max_moves = max_moves
         self.auto_generate = auto_generate
-        self.image = image
-        self.children = []
+        self.images = images
+        self.start_max_moves = start_max_moves
+        self.has_capturing = has_capturing
+        if has_capturing:
+            if capturing is None:
+                raise ValueError("no capturing given")
+            else:
+                self.capturing = capturing
+        return self
     def get_in_nd(self,n):
         directions = []
+        if self.has_capturing:
+            capturing = []
+        else:
+            capturing = None
         if self.auto_generate:
             for direction in self.directions:
-                for sign in itertools.product((True,False),repeat=direction.shape[0]):
-                    sdirection = numpy.negative(direction,where=sign,out=direction.copy())
-                    for i in itertools.permutations(range(n),sdirection.shape[0]):
-                        new_direction = numpy.zeros((n,),dtype=sdirection.dtype)
-                        new_direction[i,] = sdirection
-                        if not ina(directions,new_direction):
-                            directions.append(new_direction)
+                for new_direction in extend(direction, n):
+                    if not ina(directions,new_direction):
+                        directions.append(new_direction)
+            if self.has_capturing:
+                for capturingn in self.capturing:
+                    for new_capturing in extend(capturingn, n):
+                        if not ina(capturing,new_capturing):
+                            capturing.append(new_capturing)
         else:
             for direction in self.directions:
                 new_direction = numpy.zeros((n,))
                 new_direction[:direction.shape[0]] = direction
                 directions.append(new_direction)
-        ret = piece(directions, self.max_moves, self.auto_generate, self.image)
+            if self.has_capturing:
+                for direction in self.capturing:
+                    new_direction = numpy.zeros((n,))
+                    new_direction[:direction.shape[0]] = direction
+                    directions.append(new_direction)
+        ret = piece.from_values(directions, self.max_moves, self.auto_generate, self.images, self.start_max_moves, self.has_capturing, capturing)
         ret.parent = self
         self.children.append(ret)
         return ret
--- src/ndchess/gui.py
+++ src/ndchess/gui.py
@@ -4,14 +4,14 @@ Created on 07.03.2019
 @author: michi
 '''
 import gi
+from pcardext import ls
+from os.path import abspath
 gi.require_version('Gtk', '3.0')
 
 from gi.repository import Gtk
 import itertools
 import ndchess
-from ndchess import ina, hashable_array
-from functools import reduce
-from operator import xor
+from ndchess import ina, hashable_array, flags
 from gi.repository import Gdk
 from gi.repository import GdkPixbuf
 import numpy
@@ -20,7 +20,21 @@ import cairo
 square_size = 20
 color_light = (1,1,1)
 color_dark = (0,0,0)
-selected_alpha = 0.5
+selected_color = (1,0,0,0.5)
+
+def draw_checkerboard(cr,l,h):
+    for x,y in itertools.product(range(l),range(h)):
+        if x%2^y%2:
+            cr.set_source_rgb(*color_light)
+        else:
+            cr.set_source_rgb(*color_dark)
+        x *= square_size
+        y *= square_size
+        cr.rectangle(x,y,square_size,square_size)
+        cr.fill()
+
+def getd(l,i,d):
+    return next(iter(l[i:]),d)
 
 def world_to_screen(wc,shape2d):
     sx = wc[0]
@@ -39,8 +53,8 @@ def screen_to_world(sc,shape2d):
     x = sc[0]//square_size
     y = sc[1]//square_size
     ux = True
-    wc = numpy.empty(len(shape2d)+2,dtype=int)
-    for i,sh in enumerate(shape2d,2):
+    wc = numpy.empty(len(shape2d),dtype=int)
+    for i,sh in enumerate(shape2d[:-2],2):
         if ux:
             wc[i] = x//sh
             x = x%sh
@@ -63,92 +77,127 @@ class lwidget(Gtk.Misc):
         width = shape[0]
         height = shape[1]
         self.shape_2d = [width]
-        if len(shape)>3:
-            height = shape[1]
-            self.shape_2d.append(height)
-            for sh in self.chess.board.shape[2:-2]:
-                if x:
-                    width = width*sh
-                    self.shape_2d.append(width)
-                    x = False
-                else:
-                    height = height*sh
-                    self.shape_2d.append(height)
-                    x = True
+        self.shape_2d.append(height)
+        for sh in self.chess.board.shape[2:]:#-2]:
+            if x:
+                width = width*sh
+                self.shape_2d.append(width)
+                x = False
+            else:
+                height = height*sh
+                self.shape_2d.append(height)
+                x = True
+        self.width = width
+        self.height = height
         self.active_player = 1
         self.players = players
         self.selected = []
         self.selected_piece = None
+        self.selected_pos = None
+        self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32,self.width*square_size,self.height*square_size)
+        self.tcr = cairo.Context(self.surface)
+        self.piece = 1
+        self.pieces = len(self.chess.allowed_pieces)-1
+        self.player = 1
+        self.nonexistent = GdkPixbuf.Pixbuf.new_from_file_at_scale(abspath("../../pieces/nonexistent.png"),square_size,square_size,False)
+        self.ctrl=False
         for i in self.chess.allowed_pieces[1:]:
-            i.pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(i.image,square_size,square_size,False)
-        self.connect("button-press-event",self.button_pressed)
+            i.pixbufs = [GdkPixbuf.Pixbuf.new_from_file_at_scale(image,square_size,square_size,False) for image in i.images[:self.players]]
     def do_draw(self,cr):
+        tcr = self.tcr
+        tcr.set_source_rgba(0.0, 0.0, 0.0, 0.0)
+        tcr.set_operator(cairo.OPERATOR_CLEAR)
+        tcr.paint()
+        tcr.set_operator(cairo.OPERATOR_OVER)
+        
         for wc in itertools.product(*map(range,self.chess.board.shape)):
-            if reduce(xor,map(lambda x:bool(x%2),wc)):
-                cr.set_source_rgb(*color_light)
-            else:
-                cr.set_source_rgb(*color_dark)
             sc = world_to_screen(wc, self.shape_2d)
-            cr.rectangle(*sc,square_size,square_size)
-            cr.fill()
             cont = self.chess.board[wc]
             if cont:
-                Gdk.cairo_set_source_pixbuf(cr,self.chess.allowed_pieces[cont["piece"]].pixbuf,0,0)
-                cr.get_source().set_matrix(cairo.Matrix(x0=-sc[0],y0=-sc[1]))
-                cr.rectangle(*sc,square_size,square_size)
-                cr.fill()
+                Gdk.cairo_set_source_pixbuf(tcr,getd(self.chess.allowed_pieces[cont["piece"]].pixbufs,cont["player"]-1,self.nonexistent),0,0)
+                tcr.get_source().set_matrix(cairo.Matrix(x0=-sc[0],y0=-sc[1]))
+                tcr.rectangle(*sc,square_size,square_size)
+                tcr.fill()
+        
         if self.selected_piece:
-            Gdk.cairo_set_source_pixbuf(cr,self.selected_piece.pixbuf,0,0)
-            cr.get_source().set_extend(cairo.EXTEND_REPEAT)
+            Gdk.cairo_set_source_pixbuf(tcr,getd(self.selected_piece.pixbufs,self.chess.board[self.selected_pos]["player"]-1,self.nonexistent),0,0)
+            tcr.get_source().set_extend(cairo.EXTEND_REPEAT)
         else:
-            cr.set_source_rgb(1,0,0)
+            tcr.set_source_rgb(1,0,0)
         for wc in self.selected:
-            cr.rectangle(*world_to_screen(wc, self.shape_2d),square_size,square_size)
-        cr.clip()
-        cr.paint_with_alpha(selected_alpha)
-        cr.reset_clip()
+            tcr.rectangle(*world_to_screen(wc, self.shape_2d),square_size,square_size)
+        tcr.clip()
+        tcr.paint()
+        tcr.set_operator(cairo.OPERATOR_ATOP)
+        tcr.set_source_rgba(*selected_color)
+        tcr.paint()
+        tcr.reset_clip()
+        draw_checkerboard(cr, self.width, self.height)
+        cr.set_source(cairo.SurfacePattern(self.surface))
+        cr.paint()
+        x = self.width*square_size
+        y = self.height*square_size
+        Gdk.cairo_set_source_pixbuf(cr,getd(self.chess.allowed_pieces[self.piece].pixbufs,self.player-1,self.nonexistent),0,0)
+        cr.get_source().set_matrix(cairo.Matrix(x0=-x,y0=-y))
+        cr.rectangle(x,y,square_size,square_size)
+        cr.fill()
+    
+    def check_ctrl(self,target,event,m):
+        if event.get_keyval().keyval==Gdk.KEY_Control_L:
+            self.ctrl = m
+    
+    def key_pressed(self,target,event):
+        key = event.get_keyval().keyval
+        if key==Gdk.KEY_Up:
+            self.piece = (self.piece%self.pieces)+1
+            self.queue_draw()
+        elif key==Gdk.KEY_Down:
+            self.piece = ((self.piece-2)%self.pieces)+1
+            self.queue_draw()
+        elif key==Gdk.KEY_Left:
+            self.player = ((self.player-2)%self.players)+1
+            self.queue_draw()
+        elif key==Gdk.KEY_Right:
+            self.player = (self.player%self.players)+1
+            self.queue_draw()
     
     def button_pressed(self,target,event):
         wc = screen_to_world((event.x,event.y), self.shape_2d)
-        if self.selected:
-            if numpy.array_equal(wc,self.selected[0]):
-                self.selected = []
-            elif ina(self.selected,wc):
-                pos = tuple(self.selected[0])
-                twc = tuple(wc)
-                self.chess.board[twc] = self.chess.board[pos]
-                self.chess.board[pos] = b""
-                if self.chess.board[twc]["piece"]==1:
-                    self.chess.king_positions.discard(self.selected[0].view(hashable_array))
-                    self.chess.king_positions.add(wc.view(hashable_array))
-                print(self.active_player)
-                self.active_player = (self.active_player%self.players)+1
-                print(self.active_player)
+        if self.ctrl:
+            twc = tuple(wc)
+            if self.chess.board[twc]["piece"]==1:
+                self.chess.king_positions.discard((wc.view(hashable_array),self.chess.board[twc]["player"]))
+            if self.piece==1:
+                self.chess.king_positions.add((wc.view(hashable_array),self.player))
+            self.chess.board[twc]["piece"] = self.piece
+            self.chess.board[twc]["player"] = self.player
+            self.chess.board[twc]["flags"] = 0
+        elif self.selected:
+            if numpy.array_equal(wc,self.selected_pos):
                 self.selected = []
                 self.selected_piece = None
+                self.selected_pos = None
+            elif ina(self.selected,wc):
+                pos = tuple(self.selected_pos)
+                twc = tuple(wc)
+                self.chess.board[twc] = self.chess.board[pos]
+                self.chess.board[twc]["flags"] |= flags.MOVED
+                self.chess.board[pos] = b""
+                if self.chess.board[twc]["piece"]==1:
+                    self.chess.king_positions.discard((self.selected_pos.view(hashable_array),self.chess.board[twc]["player"]))
+                    self.chess.king_positions.add(wc.view(hashable_array))
+                self.active_player = (self.active_player%self.players)+1
+                self.selected = []
+                self.selected_piece = None
+                self.selected_pos = None
         else:
-            cont = self.chess.board[tuple(wc)]
-            print(self.active_player)
-            if cont["piece"] and cont["player"]==self.active_player:
-                self.selected.append(wc)
-                piece = self.chess.allowed_pieces[cont["piece"]]
-                self.selected_piece = piece
-                for i in piece.directions:
-                    j = 1
-                    while j<=piece.max_moves:
-                        pos = wc+i*j
-                        if (pos<0).any():
-                            break
-                        try:
-                            cont2 = self.chess.board[tuple(pos)]
-                        except IndexError:
-                            break
-                        if cont2["player"]==self.active_player:
-                            break
-                        self.selected.append(pos)
-                        if cont2["piece"]:
-                            break
-                        j+=1
+            it = self.chess.get_all_moves(wc,self.active_player)
+            try:
+                self.selected_piece = next(it)
+                self.selected = list(it)
+                self.selected_pos = wc
+            except StopIteration:
+                pass
         self.queue_draw()
 
 class widget(Gtk.EventBox):
@@ -156,17 +205,25 @@ class widget(Gtk.EventBox):
         Gtk.EventBox.__init__(self)
         self.cwidget = lwidget(chess)
         self.add(self.cwidget)
+        #self.add_events(Gdk.EventMask.KEY_PRESS_MASK)
+        #self.add_events(Gdk.EventMask.KEY_RELEASE_MASK)
+        self.set_can_focus(True)
         self.connect("button-press-event",self.cwidget.button_pressed)
+        self.connect("key-press-event",self.cwidget.check_ctrl,True)
+        self.connect("key-press-event",self.cwidget.key_pressed)
+        self.connect("key-release-event",self.cwidget.check_ctrl,False)
     
 
 class window(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self)
         self.add_events(Gdk.EventMask.BUTTON_PRESS_MASK)
-        self.cwidget = widget(ndchess.ndChess((7,7,2,2),[ndchess.piece([numpy.array([1])], float("inf"),image="image.png")]*2))
-        self.cwidget.cwidget.chess.place_piece((0,0,0,0),2,2)
-        self.cwidget.cwidget.chess.place_piece((0,1,0,0),1,2)
-        self.cwidget.cwidget.chess.place_piece((1,2,0,0),1,1)
+        self.add_events(Gdk.EventMask.KEY_PRESS_MASK)
+        self.add_events(Gdk.EventMask.KEY_RELEASE_MASK)
+        self.cwidget = widget(ndchess.ndChess((7,7,2,2),ndchess.pieces_from_file("../../pieces/default.json")))
+        #self.cwidget.cwidget.chess.place_piece((0,0,0,0),2,2)
+        #self.cwidget.cwidget.chess.place_piece((0,1,0,0),1,2)
+        #self.cwidget.cwidget.chess.place_piece((1,2,0,0),1,1)
         self.add(self.cwidget)
 
 if __name__=="__main__":
